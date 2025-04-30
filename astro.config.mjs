83912d648016f8f5070b9f3b205465597c4db49d
Enk-- <pbvinge@gmail.com>
2025-04-29 19:23:36 -0500

    trying tailwind css to fix my issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   site: 'https://pbvinge.com',
-  output: 'static'
+  output: 'static',
+  integrations: [tailwind()]
 });
